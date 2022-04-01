f50747d770f6fbc83c397c53d0fa1ddca45ff9eb
ByronMonge <byron.monge.est@tecazuay.edu.ec>
2022-04-01 18:38:10 -0500

    Insert Alergias

---
+++ Asilo_anciano_1.0/src/conexion_bada/Insert_alergias.java
@@ -0,0 +1,41 @@
+package conexion_bada;
+
+import clases.alergias;
+import conexion_bada.Conexion;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Insert_alergias extends alergias {
+
+    Conexion cone = new Conexion();
+
+    public boolean InsertarAlergias() {
+        String sql = "INSERT INTO alergias(\n"
+                + "	aler_nombre_)\n"
+                + "	VALUES ('" + getNombre_alergia() + "');";
+        return cone.InsertUpdateDeleteAcciones(sql);
+    }
+
+    public List<alergias> ListaAlergias() {
+        String sqls = "select * from alergias;";
+        ResultSet rs = cone.selectConsulta(sqls);
+        List<alergias> alergia = new ArrayList<>();
+        try {
+            while (rs.next()) {
+                alergias mi_alergia = new alergias();
+                mi_alergia.setCodigo_alergia(rs.getString("Codigo"));
+                mi_alergia.setNombre_alergia(rs.getString("Alergia"));
+                alergia.add(mi_alergia);
+            }
+            rs.close();
+            return alergia;
+        } catch (SQLException ex) {
+            Logger.getLogger(Insert.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+}
