855e157e2c5e210d4f7037ab9a94e0c99455bd03
Bryan Curillo <bryan.curillo.est@tecazuay.edu.ec>
2022-04-02 18:06:21 -0500

    arreglar combo tipo sangre

--- Asilo_anciano_1.0/src/ventanas/Agregar_paciente.java
+++ Asilo_anciano_1.0/src/ventanas/Agregar_paciente.java
@@ -232,7 +232,7 @@ public class Agregar_paciente extends javax.swing.JFrame {
 
         jLabel10.setText("Tipo de sangre");
 
-        combo_sangre_paciente.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-" }));
+        combo_sangre_paciente.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Seleccione una opcion", "A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-" }));
 
         jLabel14.setText("Dirección:");
 
@@ -354,7 +354,7 @@ public class Agregar_paciente extends javax.swing.JFrame {
                                             .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
                                             .addGap(344, 344, 344)))
                                     .addComponent(Guardar_paciente, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 19, Short.MAX_VALUE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                 .addComponent(text_codigo_paciente, javax.swing.GroupLayout.PREFERRED_SIZE, 148, javax.swing.GroupLayout.PREFERRED_SIZE))
                             .addGroup(layout.createSequentialGroup()
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
