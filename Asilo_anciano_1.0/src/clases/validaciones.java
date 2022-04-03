cf2bec9f2d99ba439c1b00d8a14d4a5214a6df1f
Bryan Curillo <bryan.curillo.est@tecazuay.edu.ec>
2022-04-03 05:08:16 -0500

    validacion usuario y contraseña

--- Asilo_anciano_1.0/src/clases/validaciones.java
+++ Asilo_anciano_1.0/src/clases/validaciones.java
@@ -15,7 +15,7 @@ import java.util.regex.Pattern;
 public class validaciones {
 
     public boolean validar_cedula(String aux) {
-        boolean validar=aux.matches("^\\d{10}$");
+        boolean validar = aux.matches("^\\d{10}$");
         return validar;
     }
 
@@ -40,18 +40,33 @@ public class validaciones {
         boolean validar = direccion.matches("([\\w\\s]+\\-*+\\#*)*");
         return validar;
     }
-    
+
     public boolean validarNombresEspacios(String cadena) {
         cadena = cadena.trim();//trim()
         boolean validar = cadena.matches("[A-Za-z\\s]*");
         return validar;
     }
-    
-     public boolean validarTelefono(String telefono) {
+
+    public boolean validarTelefono(String telefono) {
         boolean validar = false;
         if (telefono.matches("[0-9]{10}")) {
-                validar = true;
+            validar = true;
         }
+        return validar;
+    }
+
+    public boolean validarContrasena(String clave) {
+        boolean validar = false;
+//        String expreg="(\"^[A-Z]{1}[\\\\d]{3}[a-z]{2}[^A-ZA-Z0-9]{1}$\")";
+        String expreg = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%.!%-_^*&+=()])(?=\\S+$).{5,20}$";//MINIMO 1 mayus y 1 minus , 1 caract especial, minimo 8 y max 20
+        //min 1 letra mayus | min 1 letra minus | min 1 caract especial | min 1 numero | minimo 5 caracteres max 20
+        validar = clave.matches(expreg);
+        return validar;
+    }
+
+    public boolean validarUsuario(String usuario) {
+        boolean validar = usuario.matches("^[a-zA-Z]{3,}[\\d]*$");
+
         return validar;
     }
 }
