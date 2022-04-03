a02af06a07d2f51cdc45deb03d8b81cc570d3cdb
Bryan Curillo <bryan.curillo.est@tecazuay.edu.ec>
2022-04-03 18:32:50 -0500

    insert persona

---
+++ Asilo_anciano_1.0/src/conexion_bada/Insert_Persona.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conexion_bada;
+
+import clases.persona;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Bryan
+ */
+public class Insert_Persona extends persona{
+    
+    Conexion cone = new Conexion();
+
+    public boolean InsertarPersona() {
+        String sql = "INSERT INTO persona(\n"
+                + "	per_cedula, per_primer_nombre,per_segundo_nombre, per_primer_apellido,per_segundo_apellido, per_correo, per_genero, per_direccion,per_tipo_sangre,per_telefono,per_fecha_nacimiento)\n"
+                + "	VALUES ('" + getCedula() + "', '" + getPri_nomb() + "', '" + getSeg_nombre() + "', '" + getPrim_apell() + "', '" + getSeg_apelli() + "', '" + getCorreo() + "', '" + getGenero() + "', '" + getDireccion() + "', '" + getTipo_sangre() + "','" + getTelefono() + "','" + getFecha_Nacimiento() + "');";
+        return cone.InsertUpdateDeleteAcciones(sql);
+    }
+    
+        public boolean validarduplicado(String cedula) throws SQLException {
+        boolean validar = false;
+        int codigo = 0;
+        String sqls = "select count(*) from persona where per_cedula='" + cedula + "';";
+        ResultSet dup = cone.selectConsulta(sqls);
+//        try {catch
+        while (dup.next()) {
+            codigo = dup.getInt("count");
+        }
+        if (codigo == 0) {
+            validar = true;
+        }
+        System.out.println("repetido=" + codigo);
+        return validar;
+    }
+}
