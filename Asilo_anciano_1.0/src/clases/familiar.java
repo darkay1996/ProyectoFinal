4ab427def5e328d199c89ba0bf9fb747d6ac29df
ByronMonge <byron.monge.est@tecazuay.edu.ec>
2022-04-02 23:05:23 -0500

    Clase familiar. Nuevos atributos

--- Asilo_anciano_1.0/src/clases/familiar.java
+++ Asilo_anciano_1.0/src/clases/familiar.java
@@ -3,27 +3,24 @@ package clases;
 public class familiar extends persona {
 
     private String codigo_de_paciente;
-    private String fecha_de_visita;
-    private String hora_inicio;
-    private String hora_fin;
+    private String parectesco;
+    private int cod_usuario;
 
     public familiar() {
         super();
     }
 
-    public familiar(String codigo_de_paciente, String fecha_de_visita, String hora_inicio, String hora_fin) {
+    public familiar(String codigo_de_paciente, String parectesco, int cod_usuario) {
         this.codigo_de_paciente = codigo_de_paciente;
-        this.fecha_de_visita = fecha_de_visita;
-        this.hora_inicio = hora_inicio;
-        this.hora_fin = hora_fin;
+        this.parectesco = parectesco;
+        this.cod_usuario = cod_usuario;
     }
 
-    public familiar(String codigo_de_paciente, String fecha_de_visita, String hora_inicio, String hora_fin, String codigo, String cedula, String pri_nomb, String seg_nombre, String prim_apell, String seg_apelli, String correo, String genero, String fecha_Nacimiento, String direccion, String telefono, String tipo_sangre) {
+    public familiar(String codigo_de_paciente, String parectesco, int cod_usuario, String codigo, String cedula, String pri_nomb, String seg_nombre, String prim_apell, String seg_apelli, String correo, String genero, String fecha_Nacimiento, String direccion, String telefono, String tipo_sangre) {
         super(codigo, cedula, pri_nomb, seg_nombre, prim_apell, seg_apelli, correo, genero, fecha_Nacimiento, direccion, telefono, tipo_sangre);
         this.codigo_de_paciente = codigo_de_paciente;
-        this.fecha_de_visita = fecha_de_visita;
-        this.hora_inicio = hora_inicio;
-        this.hora_fin = hora_fin;
+        this.parectesco = parectesco;
+        this.cod_usuario = cod_usuario;
     }
 
     public String getCodigo_de_paciente() {
@@ -34,29 +31,19 @@ public class familiar extends persona {
         this.codigo_de_paciente = codigo_de_paciente;
     }
 
-    public String getFecha_de_visita() {
-        return fecha_de_visita;
+    public String getParectesco() {
+        return parectesco;
     }
 
-    public void setFecha_de_visita(String fecha_de_visita) {
-        this.fecha_de_visita = fecha_de_visita;
+    public void setParectesco(String parectesco) {
+        this.parectesco = parectesco;
     }
 
-    public String getHora_inicio() {
-        return hora_inicio;
+    public int getCod_usuario() {
+        return cod_usuario;
     }
 
-    public void setHora_inicio(String hora_inicio) {
-        this.hora_inicio = hora_inicio;
+    public void setCod_usuario(int cod_usuario) {
+        this.cod_usuario = cod_usuario;
     }
-
-    public String getHora_fin() {
-        return hora_fin;
-    }
-
-    public void setHora_fin(String hora_fin) {
-        this.hora_fin = hora_fin;
-    }
-    
-    
 }
