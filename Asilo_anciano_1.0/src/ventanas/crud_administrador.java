49d2f3ed1de1ce20eb4ef5d94f1f8851903063dd
Bryan Curillo <bryan.curillo.est@tecazuay.edu.ec>
2022-04-03 17:48:58 -0500

    listar y buscar actualizado

--- Asilo_anciano_1.0/src/ventanas/crud_administrador.java
+++ Asilo_anciano_1.0/src/ventanas/crud_administrador.java
@@ -8,7 +8,10 @@ package ventanas;
 import clases.administrador;
 import clases.paciente;
 import conexion_bada.Insert_administrador;
+import java.awt.Color;
+import java.util.ArrayList;
 import java.util.List;
+import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 
 /**
@@ -18,6 +21,7 @@ import javax.swing.table.DefaultTableModel;
 public class crud_administrador extends javax.swing.JFrame {
 
     Insert_administrador inser = new Insert_administrador();
+
     public crud_administrador() {
         initComponents();
         cargarTabla();
@@ -36,8 +40,9 @@ public class crud_administrador extends javax.swing.JFrame {
         BtRegresarAdministrador = new javax.swing.JButton();
         BtEliminarAdministrador = new javax.swing.JButton();
         BtEditarAdministrador = new javax.swing.JButton();
+        jButton1 = new javax.swing.JButton();
         BtBuscarAdministrador = new javax.swing.JButton();
-        jTextField1 = new javax.swing.JTextField();
+        text_buscar = new javax.swing.JTextField();
         BtIngresarAdministrador = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         TablaAdministrador = new javax.swing.JTable();
@@ -65,10 +70,21 @@ public class crud_administrador extends javax.swing.JFrame {
                 BtEliminarAdministradorActionPerformed(evt);
             }
         });
-        getContentPane().add(BtEliminarAdministrador, new org.netbeans.lib.awtextra.AbsoluteConstraints(800, 100, -1, -1));
+        getContentPane().add(BtEliminarAdministrador, new org.netbeans.lib.awtextra.AbsoluteConstraints(740, 100, -1, -1));
 
         BtEditarAdministrador.setText("EDITAR ADMINISTRADOR");
-        getContentPane().add(BtEditarAdministrador, new org.netbeans.lib.awtextra.AbsoluteConstraints(620, 100, -1, -1));
+        getContentPane().add(BtEditarAdministrador, new org.netbeans.lib.awtextra.AbsoluteConstraints(570, 100, -1, -1));
+
+        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/ACTUALIZARTAB.jpeg"))); // NOI18N
+        jButton1.setToolTipText("Ver todos");
+        jButton1.setBorderPainted(false);
+        jButton1.setOpaque(false);
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(930, 90, 40, 40));
 
         BtBuscarAdministrador.setText("BUSCAR ADMINISTRADOR");
         BtBuscarAdministrador.addActionListener(new java.awt.event.ActionListener() {
@@ -76,8 +92,15 @@ public class crud_administrador extends javax.swing.JFrame {
                 BtBuscarAdministradorActionPerformed(evt);
             }
         });
-        getContentPane().add(BtBuscarAdministrador, new org.netbeans.lib.awtextra.AbsoluteConstraints(440, 100, -1, -1));
-        getContentPane().add(jTextField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 100, 150, -1));
+        getContentPane().add(BtBuscarAdministrador, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 100, -1, -1));
+
+        text_buscar.setText("Buscar...");
+        text_buscar.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                text_buscarMousePressed(evt);
+            }
+        });
+        getContentPane().add(text_buscar, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 100, 150, -1));
 
         BtIngresarAdministrador.setText("INGRESAR ADMINISTRADOR");
         BtIngresarAdministrador.addActionListener(new java.awt.event.ActionListener() {
@@ -85,14 +108,14 @@ public class crud_administrador extends javax.swing.JFrame {
                 BtIngresarAdministradorActionPerformed(evt);
             }
         });
-        getContentPane().add(BtIngresarAdministrador, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 100, -1, -1));
+        getContentPane().add(BtIngresarAdministrador, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 100, -1, -1));
 
         TablaAdministrador.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
 
             },
             new String [] {
-                "Codigo", "Cédula", "Primer nombre", "segundo nombre", "Primer apellido", "Segundo apellido", "Sexo", "Tipo de sangre", "Dirección", "Fecha de nacimiento", "Nivel de educacion", "E-mail", "Celular"
+                "Codigo", "Cédula", "Primer nombre", "segundo nombre", "Primer apellido", "Segundo apellido", "Genero", "Tipo de sangre", "Dirección", "Fecha de nacimiento", "Nivel de educacion", "E-mail", "Celular"
             }
         ));
         jScrollPane1.setViewportView(TablaAdministrador);
@@ -115,7 +138,11 @@ public class crud_administrador extends javax.swing.JFrame {
     }//GEN-LAST:event_BtEliminarAdministradorActionPerformed
 
     private void BtBuscarAdministradorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BtBuscarAdministradorActionPerformed
-        // TODO add your handling code here:
+        if (!text_buscar.getText().isEmpty()) {
+            buscar_admin();
+        } else {
+            JOptionPane.showMessageDialog(this, "Ingrese la cedula del administrador");
+        }
     }//GEN-LAST:event_BtBuscarAdministradorActionPerformed
 
     private void BtIngresarAdministradorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BtIngresarAdministradorActionPerformed
@@ -123,17 +150,64 @@ public class crud_administrador extends javax.swing.JFrame {
         new Agregar_paciente().setVisible(true);
     }//GEN-LAST:event_BtIngresarAdministradorActionPerformed
 
-    
-        public void cargarTabla() {
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        cargarTabla();
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void text_buscarMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_text_buscarMousePressed
+        text_buscar.setText("");
+        text_buscar.setForeground(Color.BLACK);    }//GEN-LAST:event_text_buscarMousePressed
+
+    public void cargarTabla() {
         DefaultTableModel tb = (DefaultTableModel) TablaAdministrador.getModel();
         tb.setNumRows(0);
         List<administrador> com = inser.ListaAdministrador();
         com.stream().forEach(p -> {
-            String[] cami = {p.getCodigo(), p.getCedula(), p.getPri_nomb(), p.getSeg_nombre(), p.getPrim_apell(), p.getSeg_apelli(), p.getGenero(), p.getTipo_sangre(), p.getDireccion(),p.getFecha_Nacimiento(), p.getNivel_educacion(), p.getCorreo(), p.getTelefono()};
+            String[] cami = {p.getCodigo(), p.getCedula(), p.getPri_nomb(), p.getSeg_nombre(), p.getPrim_apell(), p.getSeg_apelli(), p.getGenero(), p.getTipo_sangre(), p.getDireccion(), p.getFecha_Nacimiento(), p.getNivel_educacion(), p.getCorreo(), p.getTelefono()};
             tb.addRow(cami);
         });
     }
-    
+
+    public void buscar_admin() {
+
+        String cedula = text_buscar.getText();
+        var adminfiltro = new ArrayList<administrador>();
+
+        inser.ListaAdministrador().forEach((a) -> {
+            if (a.getCedula().equals(cedula)) {
+                adminfiltro.add(a);
+            }
+        });
+        if (adminfiltro.size() != 0) {
+            String matriz[][] = new String[adminfiltro.size()][14];
+            for (int j = 0; j < adminfiltro.size(); j++) {
+                matriz[j][0] = adminfiltro.get(j).getCodigo();
+                matriz[j][1] = adminfiltro.get(j).getCedula();
+                matriz[j][2] = adminfiltro.get(j).getPri_nomb();
+                matriz[j][3] = adminfiltro.get(j).getSeg_nombre();
+                matriz[j][4] = adminfiltro.get(j).getPrim_apell();
+                matriz[j][5] = adminfiltro.get(j).getSeg_apelli();
+                matriz[j][6] = adminfiltro.get(j).getGenero();
+                matriz[j][7] = adminfiltro.get(j).getTipo_sangre();
+                matriz[j][8] = adminfiltro.get(j).getDireccion();
+                matriz[j][9] = adminfiltro.get(j).getFecha_Nacimiento();
+                matriz[j][10] = adminfiltro.get(j).getNivel_educacion();
+                matriz[j][11] = adminfiltro.get(j).getCorreo();
+                matriz[j][12] = adminfiltro.get(j).getTelefono();
+
+            }
+            TablaAdministrador.setModel(new javax.swing.table.DefaultTableModel(
+                    matriz,
+                    new String[]{
+                        "Codigo", "Cédula", "Primer nombre", "segundo nombre", "Primer apellido", "Segundo apellido", "Genero", "Tipo de sangre", "Dirección", "Fecha de nacimiento", "Nivel de educacion", "E-mail", "Celular"
+                    }
+            ));
+        } else {
+            JOptionPane.showMessageDialog(this, "El administrador no existe en la base de datos");
+        }
+
+    }
+
     /**
      * @param args the command line arguments
      */
@@ -177,8 +251,9 @@ public class crud_administrador extends javax.swing.JFrame {
     private javax.swing.JButton BtRegresarAdministrador;
     private javax.swing.JLabel Fondo;
     private javax.swing.JTable TablaAdministrador;
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField text_buscar;
     // End of variables declaration//GEN-END:variables
 }
