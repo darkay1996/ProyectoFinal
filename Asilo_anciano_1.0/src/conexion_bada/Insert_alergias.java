ce89f5d374f92a6c590eaa5ba908361911731fb9
ByronMonge <byron.monge.est@tecazuay.edu.ec>
2022-04-02 20:09:23 -0500

    Insert Alergias

--- Asilo_anciano_1.0/src/conexion_bada/Insert_alergias.java
+++ Asilo_anciano_1.0/src/conexion_bada/Insert_alergias.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
+// Prueba
 public class Insert_alergias extends alergias {
 
     Conexion cone = new Conexion();
