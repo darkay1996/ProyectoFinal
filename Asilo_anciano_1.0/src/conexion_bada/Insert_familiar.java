a8e466619fa85c5343ecc6ce0b8a53b7e89be8dd
Bryan Curillo <bryan.curillo.est@tecazuay.edu.ec>
2022-04-03 21:56:18 -0500

    cargar codigo

--- Asilo_anciano_1.0/src/conexion_bada/Insert_familiar.java
+++ Asilo_anciano_1.0/src/conexion_bada/Insert_familiar.java
@@ -10,17 +10,17 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class Insert_familiar extends familiar{
+public class Insert_familiar extends familiar {
 
     Conexion cone = new Conexion();
 
     public boolean InsertarFamiliar() {
         String sql = "INSERT INTO familiar(\n"
                 + "	fam_codigo_visit, fam_cedula,fam_primer_nombre, fam_segundo_nombre,fam_primer_apellido, fam_segundo_apellido, fam_correo,fam_genero, fam_direccion,fam_tipo_sangre,fam_cod_paciente,fam_hora_inicio_visit,fam_hora_fin_visit,fam_telefono,fam_fecha_nacimiento,fam_fecha_visit)\n"
-                + "	VALUES ('" + getCodigo() + "', '" + getCedula() + "', '" + getPri_nomb() + "', '" + getSeg_nombre() + "', '" + getPrim_apell() + "', '" + getSeg_apelli() + "', '" + getCorreo() + "', '" + getGenero() + "', '" + getDireccion() + "','" + getTipo_sangre() + "','" + getCodigo_de_paciente() + "','" +"','" + getTelefono() + "','" + getFecha_Nacimiento()+ "');";
+                + "	VALUES ('" + getCodigo() + "', '" + getCedula() + "', '" + getPri_nomb() + "', '" + getSeg_nombre() + "', '" + getPrim_apell() + "', '" + getSeg_apelli() + "', '" + getCorreo() + "', '" + getGenero() + "', '" + getDireccion() + "','" + getTipo_sangre() + "','" + getCodigo_de_paciente() + "','" + "','" + getTelefono() + "','" + getFecha_Nacimiento() + "');";
         return cone.InsertUpdateDeleteAcciones(sql);
     }
-    
+
     public List<familiar> ListaFamiliar() {
         String sqls = "select * from familiar;";
         ResultSet rs = cone.selectConsulta(sqls);
@@ -35,16 +35,15 @@ public class Insert_familiar extends familiar{
                 mi_famili.setPrim_apell(rs.getString("fam_primer_apellido"));
                 mi_famili.setSeg_apelli(rs.getString("fam_segundo_apellido"));
                 mi_famili.setCorreo(rs.getString("fam_correo"));
-                mi_famili.setGenero(rs.getString("fam_genero")); 
+                mi_famili.setGenero(rs.getString("fam_genero"));
                 mi_famili.setDireccion(rs.getString("fam_direccion"));
                 mi_famili.setTipo_sangre(rs.getString("fam_tipo_sangre"));
-                
+
                 mi_famili.setCodigo_de_paciente(rs.getString("fam_cod_paciente"));
 
                 mi_famili.setTelefono(rs.getString("fam_telefono"));
                 mi_famili.setFecha_Nacimiento(rs.getString("fam_fecha_nacimiento"));
 
-                
                 famili.add(mi_famili);
             }
             rs.close();
@@ -54,4 +53,5 @@ public class Insert_familiar extends familiar{
             return null;
         }
     }
+
 }
