9d37f529f07bb1852b623b387e6b8c5af9dc8615
ByronMonge <byron.monge.est@tecazuay.edu.ec>
2022-04-03 22:54:33 -0500

    Insert Familiar

--- Asilo_anciano_1.0/src/conexion_bada/Insert_familiar.java
+++ Asilo_anciano_1.0/src/conexion_bada/Insert_familiar.java
@@ -14,35 +14,43 @@ public class Insert_familiar extends familiar {
 
     Conexion cone = new Conexion();
 
+    public boolean InsertarPersona() {
+        String sql = "INSERT INTO persona(\n"
+                + "	per_cedula, per_primer_nombre,per_segundo_nombre, per_primer_apellido,per_segundo_apellido, per_correo, per_genero, per_direccion,per_tipo_sangre,per_telefono,per_fecha_nacimiento)\n"
+                + "	VALUES ('" + getCedula() + "', '" + getPri_nomb() + "', '" + getSeg_nombre() + "', '" + getPrim_apell() + "', '" + getSeg_apelli() + "', '" + getCorreo() + "', '" + getGenero() + "', '" + getDireccion() + "', '" + getTipo_sangre() + "','" + getTelefono() + "','" + getFecha_Nacimiento() + "');";
+        return cone.InsertUpdateDeleteAcciones(sql);
+    }
+
     public boolean InsertarFamiliar() {
         String sql = "INSERT INTO familiar(\n"
-                + "	fam_codigo_visit, fam_cedula,fam_primer_nombre, fam_segundo_nombre,fam_primer_apellido, fam_segundo_apellido, fam_correo,fam_genero, fam_direccion,fam_tipo_sangre,fam_cod_paciente,fam_hora_inicio_visit,fam_hora_fin_visit,fam_telefono,fam_fecha_nacimiento,fam_fecha_visit)\n"
-                + "	VALUES ('" + getCodigo() + "', '" + getCedula() + "', '" + getPri_nomb() + "', '" + getSeg_nombre() + "', '" + getPrim_apell() + "', '" + getSeg_apelli() + "', '" + getCorreo() + "', '" + getGenero() + "', '" + getDireccion() + "','" + getTipo_sangre() + "','" + getCodigo_de_paciente() + "','" + "','" + getTelefono() + "','" + getFecha_Nacimiento() + "');";
+                + "	fam_cedula,fam_parentesco,fam_codigo_usuario)\n"
+                + "	VALUES ('" + getCedula() + "', '" + getParectesco() + "', '" + getCod_usuario() + "');";
         return cone.InsertUpdateDeleteAcciones(sql);
     }
 
     public List<familiar> ListaFamiliar() {
-        String sqls = "select * from familiar;";
+        String sqls = "select * from persona per, familiar fami, usuario us where per.per_cedula= fami.fam_cedula and  us.us_codigo = fam_codigo_usuario;";
         ResultSet rs = cone.selectConsulta(sqls);
         List<familiar> famili = new ArrayList<>();
         try {
             while (rs.next()) {
                 familiar mi_famili = new familiar();
-                mi_famili.setCodigo(rs.getString("fam_codigo_visit"));
+                //mi_famili.setCodigo(rs.getString("fam_codigo_visit"));
+                mi_famili.setCodigo(rs.getString("fam_codigo"));
                 mi_famili.setCedula(rs.getString("fam_cedula"));
-                mi_famili.setPri_nomb(rs.getString("fam_primer_nombre"));
-                mi_famili.setSeg_nombre(rs.getString("fam_segundo_nombre"));
-                mi_famili.setPrim_apell(rs.getString("fam_primer_apellido"));
-                mi_famili.setSeg_apelli(rs.getString("fam_segundo_apellido"));
-                mi_famili.setCorreo(rs.getString("fam_correo"));
-                mi_famili.setGenero(rs.getString("fam_genero"));
-                mi_famili.setDireccion(rs.getString("fam_direccion"));
-                mi_famili.setTipo_sangre(rs.getString("fam_tipo_sangre"));
+                mi_famili.setParectesco(rs.getString("fam_parentesco,"));
+                mi_famili.setCod_usuario(rs.getInt("fam_codigo_usuario"));
 
-                mi_famili.setCodigo_de_paciente(rs.getString("fam_cod_paciente"));
-
-                mi_famili.setTelefono(rs.getString("fam_telefono"));
-                mi_famili.setFecha_Nacimiento(rs.getString("fam_fecha_nacimiento"));
+                mi_famili.setPri_nomb(rs.getString("per_primer_nombre"));
+                mi_famili.setSeg_nombre(rs.getString("per_segundo_nombre"));
+                mi_famili.setPrim_apell(rs.getString("per_primer_apellido"));
+                mi_famili.setSeg_apelli(rs.getString("per_segundo_apellido"));
+                mi_famili.setCorreo(rs.getString("per_correo"));
+                mi_famili.setGenero(rs.getString("per_genero"));
+                mi_famili.setFecha_Nacimiento(rs.getString("per_fecha_nacimiento"));
+                mi_famili.setDireccion(rs.getString("per_direccion"));
+                mi_famili.setTelefono(rs.getString("per_telefono"));
+                mi_famili.setTipo_sangre(rs.getString("per_tipo_sangre"));
 
                 famili.add(mi_famili);
             }
@@ -54,17 +62,20 @@ public class Insert_familiar extends familiar {
         }
     }
 
-    public int cargarcodigo() {
+    public boolean validarduplicado(String cedula) throws SQLException {
+        boolean validar = false;
         int codigo = 0;
-        String sqls = "select max(fam_codigo) from familiar;";
-        ResultSet ru = cone.selectConsulta(sqls);
-        try {
-            while (ru.next()) {
-                codigo = ru.getInt("max") + 1;
-            }
-        } catch (SQLException ex) {
-            Logger.getLogger(Insert.class.getName()).log(Level.SEVERE, null, ex);
+        String sqls = "select count(*) from persona where per_cedula='" + cedula + "';";
+        ResultSet dup = cone.selectConsulta(sqls);
+//        try {catch
+        while (dup.next()) {
+            codigo = dup.getInt("count");
         }
-        return codigo;
+        if (codigo == 0) {
+            validar = true;
+        }
+
+        System.out.println("repetido=" + codigo);
+        return validar;
     }
 }
